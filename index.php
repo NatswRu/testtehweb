a5148ffb0811c8d8eeac13071cdebd05f9c62ea6
NatswRu <natsw.zero@gmail.com>
2024-09-12 10:02:38 +0500

    php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello world";
+// Hello world
+$a = 5;
+$b = '5';
+
+echo $a + $b . "<br>";
+
+echo -4 - -5;
+
+const A = 54;
+
+echo A;
+/*
+echo "Hello world" . "<br>";
+echo 5+5 . "<br>";
+echo 5 . " five" . "<br>";
+echo 5**5;
+*/
+
 ?>
